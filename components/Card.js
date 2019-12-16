64edbe54d728db2f19499ce66d989cb163ea18d2
Andres Noboa <andresnoboa17@hotmail.com>
2019-12-16 12:15:44 -0500

    Created a separate component card that contain the styling for reusing in diferent parts of the app.

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import {View,Text,TextInput,StyleSheet,Button} from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 
-const Card=(props)=>{
-
+const Card = (props) => {
+    return <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
 }
-const styles=StyleSheet.create({
-
+const styles = StyleSheet.create({
+    card: {
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        elevation: 9,
+        padding: 20,
+        borderRadius: 10
+    }
 });
 
 export default Card;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { View, Text,Button, TextInput,StyleSheet } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
+import Card from '../components/Card';
 
 const StartGameScreen = (props) => {
     return (
         <View style={styles.screen}>
             <Text styles={styles.title}>Start A New Game!</Text>
-            <View style={styles.inputContainer}>
+            <Card style={styles.inputContainer}>
                 <Text>Select A Number</Text>
-                <TextInput/>
+                <TextInput />
                 <View style={styles.buttonContainer}>
-                    <Button title="Reset" onPress={()=>{}}/>
-                    <Button title="Confirm" onPress={()=>{}}/>
+                    <Button title="Reset" onPress={() => { }} />
+                    <Button title="Confirm" onPress={() => { }} />
                 </View>
-            </View>
+            </Card>
         </View>
     );
 }
@@ -23,29 +24,20 @@ const styles = StyleSheet.create({
         padding: 10,
         alignItems: 'center'
     },
-    title:{
-        fontSize:40,
-        marginVertical:10,
+    title: {
+        fontSize: 40,
+        marginVertical: 10,
     },
-    inputContainer:{
-        width:300,
-        maxWidth:'80%',
-        alignItems:'center',
-        shadowColor:'black',
-        shadowOffset:{width:0,height:2},
-        shadowRadius:6,
-        shadowOpacity:0.26,
-        backgroundColor:'white',
-        elevation:9,
-        padding:20,
-        borderRadius:10
-
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
     },
-    buttonContainer:{
-        flexDirection:'row',
-        width:'100%',
-        justifyContent:'space-between',
-        paddingHorizontal:15
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 });
 
