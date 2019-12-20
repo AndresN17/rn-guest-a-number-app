27394ee6b0319ea2e2329afcda7315f0d8ae48fc
Andres Noboa <andresnoboa17@hotmail.com>
2019-12-20 11:13:12 -0500

    Added validation so the input can only be a integer number and also added an autoclosable keyboard.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import Card from '../components/Card';
-import Colors from '../constants/colors'; 
+import Colors from '../constants/colors';
 import Input from '../components/Input';
 
 const StartGameScreen = (props) => {
+    const [enteredValue, setEnteredValue] = useState('');
+    const numberInputHandler = (inputNumber) => {
+        setEnteredValue(inputNumber.replace(/[^0-9]/g, ''));
+    };
+
     return (
-        <View style={styles.screen}>
-            <Text styles={styles.title}>Start A New Game!</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select A Number</Text>
-                <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType='numeric' maxLength={2}/>
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}>
-                        <Button title="Reset" onPress={() => { }} color={Colors.accent} />
+        <TouchableWithoutFeedback>
+            <View style={styles.screen}>
+                <Text styles={styles.title}>Start A New Game!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select A Number</Text>
+                    <Input style={styles.input}
+                        blurOnSubmit
+                        autoCapitalize='none'
+                        autoCorrect={false}
+                        keyboardType='number-pad'
+                        maxLength={2}
+                        onChangeText={numberInputHandler}
+                        value={enteredValue} />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button title="Reset" onPress={() => { }} color={Colors.accent} />
+                        </View>
+                        <View style={styles.button}>
+                            <Button title="Confirm" onPress={() => { }} color={Colors.primary} />
+                        </View>
                     </View>
-                    <View style={styles.button}>
-                        <Button title="Confirm" onPress={() => { }} color={Colors.primary} />
-                    </View>
-                </View>
-            </Card>
-        </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
+
     );
 }
 
@@ -31,7 +46,7 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     title: {
-        fontSize: 40,
+        fontSize: 70,
         marginVertical: 10,
     },
     inputContainer: {
@@ -48,9 +63,9 @@ const styles = StyleSheet.create({
     button: {
         width: "40%"
     },
-    input:{
-        width:50,
-        textAlign:'center'
+    input: {
+        width: 50,
+        textAlign: 'center'
     },
 
 });
